46e8662f795aa460871f6b52434fee7541beafcd
iee1020 <47474071+iee1020@users.noreply.github.com>
2019-07-30 22:02:47 +0900

    Delete index.js
    index.js 1.0.0 완료 후 수정 예정

--- index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { Userinfo } from './src/components/signup/userinfo.tsx';
-
-console.disableYellowBox = true;
-AppRegistry.registerComponent('tshopequery', () => Userinfo);
