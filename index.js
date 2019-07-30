843862d51f5538822db922b8f2ec9cc53a753c6e
iee1020 <47474071+iee1020@users.noreply.github.com>
2019-07-30 21:21:47 +0900

    Delete index.js
    최종 1.0.0이 될 때 변경 될 예정.

--- index.js
+++
@@ -1,5 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { ProfileRoot } from './src/components/signup/profile-root.tsx';
-
-console.disableYellowBox = true;
-AppRegistry.registerComponent('tshopequery', () => ProfileRoot);
