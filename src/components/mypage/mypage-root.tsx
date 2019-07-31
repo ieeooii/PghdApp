5d97eb796203b5fa3f65c5caefcf4a4226e8302c
iee1020 <yl9536@gmail.com>
2019-07-31 20:46:29 +0900

    feature/#13-pghd-record-data mypage-root.tsx-styles-move-in-style.tsx

--- src/components/mypage/mypage-root.tsx
+++ src/components/mypage/mypage-root.tsx
@@ -1,55 +1,11 @@
 import { Button } from 'native-base';
 import * as React from 'react';
-import { Platform, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Platform, ScrollView, Text, View } from 'react-native';
+import { mypageRootStyles } from '../style';
 import { MiniProfile } from './mini-profile';
 import { PghdRecord } from './pghdrecord';
-// import { styles } from './style';
 
-const mainColor: string = '#5800ff';
-// const andsigninbtn: string = '#343434';
-
-// const logourl: string = 'https://humanscape.io/images/logo-black.png';
-
-const styles = StyleSheet.create({
-  shadow: {
-    ...Platform.select({
-      ios: {
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 5,
-        },
-        shadowRadius: 20,
-        shadowOpacity: 0.1,
-      },
-      android: {
-        elevation: 15,
-      },
-    }),
-  },
-  mainButton: {
-    width: '90%',
-    height: 60,
-    overflow: 'hidden',
-    ...Platform.select({
-      ios: {
-        borderRadius: 50,
-      },
-      android: {
-        borderRadius: 10,
-      },
-    }),
-    marginTop: 20,
-    marginBottom: 10,
-    justifyContent: 'center',
-    backgroundColor: mainColor,
-  },
-  todayRecord: {
-    color: 'white',
-    textAlign: 'center',
-    fontSize: 19,
-  },
-});
+const styles = mypageRootStyles;
 
 export interface Props {}
 export interface State {}
@@ -57,16 +13,12 @@ export interface State {}
 export class MypageRoot extends React.Component<Props, State> {
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: '#F0F0F5',
-          flexDirection: 'column',
-        }}
-      >
+      <View style={styles.container}>
         <ScrollView>
           <MiniProfile />
-          <View style={[styles.shadow, { alignItems: 'center' }]}>
+          <View
+            style={[styles.shadow, { alignItems: 'center', marginBottom: 20 }]}
+          >
             <Button
               style={
                 Platform.OS === 'ios'
