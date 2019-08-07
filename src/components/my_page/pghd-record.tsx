723d9eb0c2e3f0bf7963fcf51b4500ee6bbd2b5a
KimJunYoung1 <46435463+KimJunYoung1@users.noreply.github.com>
2019-08-07 20:22:49 +0900

    Update my_page

--- src/components/my_page/pghd-record.tsx
+++ src/components/my_page/pghd-record.tsx
@@ -1,60 +1,133 @@
-import { Button } from 'native-base';
+import { Button, Fab, Form } from 'native-base';
 import * as React from 'react';
-import {
-  Image,
-  Platform,
-  // StyleSheet,
-  Text,
-  View,
-  YellowBox,
-} from 'react-native';
-
+import { Alert, Image, Platform, Text } from 'react-native';
 import { myprofileStyles, pghdRecordStyles } from '../style';
 
 const styles = pghdRecordStyles;
 
-export class PghdRecord extends React.Component {
+interface State {
+  activebutton?: boolean;
+}
+
+interface Props {
+  navi?: any;
+}
+
+export class PghdRecord extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
-    YellowBox.ignoreWarnings([
-      'Warning: isMounted(...) is deprecated',
-      'Module RCTImageLoader',
-    ]);
+    this.state = {
+      activebutton: false,
+    };
+  }
+
+  deleteAlert = () => {
+    Alert.alert(
+      '삭제하시겠습니까?',
+      '',
+      [
+        {
+          text: '삭제',
+          onPress: () => {},
+        },
+        {
+          text: '취소',
+          onPress: () => {},
+          style: 'cancel',
+        },
+      ],
+      { cancelable: false },
+    );
+  }
+
+  selectAlert = () => {
+    Alert.alert(
+      '수정 또는 삭제 버튼을 클릭해주세요.',
+      '',
+      [
+        {
+          text: '수정',
+          onPress: () => {
+            this.props.navi.navigation.navigate('TodayPghd');
+          },
+        },
+        {
+          text: '삭제',
+          onPress: this.deleteAlert,
+        },
+      ],
+      { cancelable: false },
+    );
   }
 
   render() {
     return (
-      <View style={{ alignItems: 'center' }}>
-        <View
+      <Form style={{ alignItems: 'center' }}>
+        <Form
           style={[
+            { zIndex: 1 },
             styles.pghdContainerTop,
             Platform.OS === 'android' ? styles.shadow : {},
           ]}
         >
-          <View style={[myprofileStyles.circle, { width: 36, height: 36 }]} />
+          <Form style={[myprofileStyles.circle, { width: 36, height: 36 }]} />
           <Text style={{ marginLeft: 10 }}>react-native</Text>
           <Text style={{ marginLeft: 5, color: '#767676' }}>| 1.10</Text>
-          <Button
+          <Fab
+            active={this.state.activebutton}
+            direction='down'
+            containerStyle={{}}
             style={[
-              { backgroundColor: 'white', position: 'absolute', right: 5 },
-              Platform.OS === 'android' ? { elevation: 0 } : {},
+              styles.fab,
+              Platform.OS === 'ios'
+                ? styles.shadowZero
+                : { backgroundColor: 'white', elevation: 0 },
             ]}
+            position='topRight'
+            onPress={
+              Platform.OS === 'ios'
+                ? () =>
+                    this.setState({ activebutton: !this.state.activebutton })
+                : this.selectAlert
+            }
           >
             <Image
-              source={require('../../../PGHD-RECORD-SELECT-ICON.png')}
-              style={{ resizeMode: 'contain', height: '45%' }}
+              source={require('./PGHD-RECORD-SELECT-ICON.png')}
+              style={{ resizeMode: 'contain', height: '40%' }}
             />
-          </Button>
-        </View>
-        <View
+            {/* <Form style={[styles.buttonForm, { width: 80, height: 80 }]}> */}
+            <Button
+              style={[
+                styles.button,
+                Platform.OS === 'android' ? { elevation: 0 } : styles.buttonForm,
+              ]}
+              onPress={() => {
+                this.props.navi.navigation.navigate('TodayPghd');
+              }}
+            >
+              <Text>수정</Text>
+            </Button>
+            <Button
+              style={[
+                styles.button,
+                Platform.OS === 'android' ? { elevation: 0 } : styles.buttonForm,
+              ]}
+              onPress={this.deleteAlert}
+            >
+              <Text>삭제</Text>
+            </Button>
+            {/* </Form> */}
+          </Fab>
+        </Form>
+        <Form
           style={[
             styles.pghdContainerBotttom,
             Platform.OS === 'ios' ? {} : styles.shadow,
           ]}
         >
           <Text style={{ textAlign: 'center' }}>일별 PGHD 기록</Text>
-        </View>
-      </View>
+        </Form>
+      </Form>
     );
   }
 }
